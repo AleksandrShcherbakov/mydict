0abd31b6f19314964457e86e9d9cbedb2bcbbe3e
AleksandrShcherbakov <Shcherbakov89@gmail.com>
2021-07-01 13:41:02 +0300

    111

--- app/src/main/java/com/home/mydict/service/PromtTranslateRequest.java
+++ app/src/main/java/com/home/mydict/service/PromtTranslateRequest.java
@@ -18,10 +18,11 @@ public class PromtTranslateRequest implements TranslateRequest {
         this.sslSocket = new SSLSocketPromt(activity);
     }
 
-    final String url = "https://www.translate.ru/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/";
+     String url = "https://www.translate.ru/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/";
 
     @Override
     public Document getHtmlPage(String word) throws IOException {
+        url="https://www.translate.ru/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/";
         Document document = Jsoup.connect(url+word).timeout(10000).sslSocketFactory(sslSocket.getSocketFactory()).get();
         return document;
     }
